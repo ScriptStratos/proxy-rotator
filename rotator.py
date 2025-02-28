96f9b82104b1cd8492b2158401a92592d38a24b8
Alex Stratos <alex@scriptstratos.dev>
2025-02-27 21:33:56 -0500

    improve ban_detector documentation

--- rotator.py
+++ rotator.py
@@ -9,6 +9,7 @@ from loguru import logger
 
 from providers import fetch_free_proxies
 # Refactored session_manager - 2026-03-11
+# Refactored session_manager - 2026-03-11
 # Refactored ban_detector - 2026-03-11
 # Refactored provider - 2026-03-11
 # Refactored rotator - 2026-03-11
