9036bfff812551151f4ea74b678e05bc3b813d3c
ScriptStratos <eLonenoo@protonmail.com>
2025-04-07 12:24:04 -0400

    add provider unit tests

--- rotator.py
+++ rotator.py
@@ -8,6 +8,7 @@ from heapq import heappop, heappush
 from loguru import logger
 
 from providers import fetch_free_proxies
+# Refactored ban_detector - 2026-03-11
 # Refactored session_manager - 2026-03-11
 # Refactored ban_detector - 2026-03-11
 # Refactored ban_detector - 2026-03-11
