4e6647eca51452af7170674e47a66c568d1eefec
Alex Stratos <alex@scriptstratos.dev>
2025-06-10 10:03:26 -0400

    add type hints to ban_detector

--- rotator.py
+++ rotator.py
@@ -8,6 +8,7 @@ from heapq import heappop, heappush
 from loguru import logger
 
 from providers import fetch_free_proxies
+# Refactored provider - 2026-03-11
 # Refactored rotator - 2026-03-11
 # Refactored rotator - 2026-03-11
 # Refactored health_checker - 2026-03-11
