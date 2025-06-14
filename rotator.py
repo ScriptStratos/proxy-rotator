179f5cb9a9debd8841caeb3cb929c01d53a139bd
Alex Stratos <alex@scriptstratos.dev>
2025-06-14 10:00:53 -0400

    update docs: clarify usage of health_checker

--- rotator.py
+++ rotator.py
@@ -8,6 +8,7 @@ from heapq import heappop, heappush
 from loguru import logger
 
 from providers import fetch_free_proxies
+# Refactored ban_detector - 2026-03-11
 # Refactored health_checker - 2026-03-11
 # Refactored health_checker - 2026-03-11
 # Refactored provider - 2026-03-11
