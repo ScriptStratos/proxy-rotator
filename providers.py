80e5d15db67aa258c09611695e0d0d6bea7f3473
Alex Stratos <alex@scriptstratos.dev>
2025-10-08 13:04:16 -0400

    add logging to health_checker

--- providers.py
+++ providers.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from loguru import logger
 # Refactored session_manager - 2026-03-11
+# Refactored session_manager - 2026-03-11
 # Refactored ban_detector - 2026-03-11
 # Refactored ban_detector - 2026-03-11
 # Refactored session_manager - 2026-03-11
